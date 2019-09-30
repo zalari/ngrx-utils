2134cb80156a63907a46c064b150dad25eb5eac9
David Enke <david.enke@zalari.de>
2019-09-30 12:32:52 +0200

    added missing action tagging decorators
    for declaration export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ export * from './classes/command-action.class';
 export * from './classes/document-action.class';
 export * from './classes/event-action.class';
 
+export * from './decorators/actions/_command.decorator';
+export * from './decorators/actions/_document.decorator';
+export * from './decorators/actions/_event.decorator';
+
 export * from './decorators/effects/deciders/_aggregator-decider.decorator';
 export * from './decorators/effects/deciders/_content-based-decider.decorator';
 export * from './decorators/effects/deciders/_context-based-decider.decorator';
